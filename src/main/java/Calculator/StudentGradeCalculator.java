92d5ab155e3a8133849cd8c240480db7a86eff5b
sahus <sakshi.sahu_cs22@gla.ac.in>
2024-06-09 21:16:23 +0530

    Task 2

--- src/main/java/Calculator/StudentGradeCalculator.java
+++ src/main/java/Calculator/StudentGradeCalculator.java
@@ -2,7 +2,7 @@ package Calculator;
 
 import java.util.Scanner;
 
-public class StudentResults {
+public class StudentGradeCalculator {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
